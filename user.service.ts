19750e3fdd3bf8d9595f849575ecf11dc3fe8a7c
Facundo Torrez <facundotorrez.50@gmail.com>
2026-01-18 21:37:18 -0300

    add user service file for testing branch

---
+++ user.service.ts
@@ -0,0 +1,4 @@
+// User service
+// This file is currently empty, but in future versions that will change
+// expanded with more details about the purpose of this NestJS project.
+// The project can be a starting point to start a venture with nestJS and adapt other projects such as Digital Card to this technology.
